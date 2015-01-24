7649244036039de309f98c074d4d4f318eaeb5f0
PanDenat <pandenat@gmail.com>
2015-01-24 17:26:21 +0100

    CameraController

---
+++ JamSiders/Assets/Camera/Editor/CameraControllerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Camera.Editor
+{
+    [CustomEditor(typeof (CameraController))]
+    internal class CameraControllerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var cameraController = (CameraController) target;
+
+            if (GUILayout.Button("Update position"))
+            {
+                cameraController.offset = cameraController.transform.position - cameraController.target.transform.position;
+                Vector3 lookAt = cameraController.target.position + cameraController.lookYoffset * Vector3.up;
+                cameraController.transform.LookAt(lookAt);
+            }
+        }
+    }
+}
