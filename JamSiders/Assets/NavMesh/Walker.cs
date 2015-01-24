c0b5a1520655a2e3b314cc70a1cf7cbf908f160e
draculeq <gsniecinski@gmail.com>
2015-01-24 16:45:41 +0100

    More dumb ai

--- JamSiders/Assets/NavMesh/Walker.cs
+++ JamSiders/Assets/NavMesh/Walker.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
 using UnityEngine;
 
-
+[SelectionBase]
 public class Walker : MonoBehaviour
 {
 
 	[SerializeField]
-	private Transform destination;
+	public Transform destination;
 	private NavMeshPath path;
 	public float Speed;
+	private new Transform transform;
+	private const float reachDestinationDistance = 2.2f;
+	public bool panicDestination;
 
 	void Awake()
 	{
 		path = new NavMeshPath();
+		transform = GetComponent<Transform>();
+		Speed += UnityEngine.Random.Range(-1f, 1f);
 	}
 	public void Update()
 	{
 		if (destination != null)
 		{
-			if ((Vector3.Distance(transform.position, destination.position) > 1f))
+			if (Vector3.Distance(transform.position, destination.position) > reachDestinationDistance)
 			{
 				if (NavMesh.CalculatePath(transform.position, destination.position, -1, path))
 				{
 					if (path.corners.Length > 1)
 						rigidbody.velocity = ((path.corners[1] - transform.position)).normalized * Speed;
 				}
-				else
-				{
-					var a = GameObject.FindGameObjectsWithTag("Kulka").ToArray();
-					SetDestination(a[UnityEngine.Random.Range(0, a.Length - 1)].transform);
-				}
 			}
-		}
-		else
-		{
-			var a = GameObject.FindGameObjectsWithTag("Kulka").ToArray();
-			SetDestination(a[UnityEngine.Random.Range(0, a.Length - 1)].transform);
+			else
+			{
+				if (panicDestination) panicDestination = false;
+			}
 		}
 	}
 
