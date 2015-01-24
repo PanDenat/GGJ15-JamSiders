4aac14449456df81ffacd2b9629ac6165a588340
draculeq <gsniecinski@gmail.com>
2015-01-24 23:48:46 +0100

    chodzenia na force

--- JamSiders/Assets/NavMesh/Walker.cs
+++ JamSiders/Assets/NavMesh/Walker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using System.Security.Cryptography;
 using UnityEngine;
@@ -11,17 +12,18 @@ public class Walker : MonoBehaviour
 	public Transform destination;
 	private NavMeshPath path;
 	public float Speed;
+	public float Acceleration;
 	private new Transform transform;
 	private const float reachDestinationDistance = 2.2f;
 	public bool panicDestination;
-
 	void Awake()
 	{
 		path = new NavMeshPath();
 		transform = GetComponent<Transform>();
 		Speed += UnityEngine.Random.Range(-1f, 1f);
 	}
-	public void Update()
+
+	public void FixedUpdate()
 	{
 		if (destination != null)
 		{
@@ -30,7 +32,10 @@ public class Walker : MonoBehaviour
 				if (NavMesh.CalculatePath(transform.position, destination.position, -1, path))
 				{
 					if (path.corners.Length > 1)
-						rigidbody.velocity = ((path.corners[1] - transform.position)).normalized * Speed;
+					{
+						if (rigidbody.velocity.magnitude<Speed)
+							rigidbody.AddForce(((path.corners[1] - transform.position)).normalized * Acceleration);
+					}
 				}
 			}
 			else
