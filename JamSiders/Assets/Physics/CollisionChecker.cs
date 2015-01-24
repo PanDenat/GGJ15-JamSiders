8463a7e8bec76141d3a8118763d3352e18466d2e
PanDenat <pandenat@gmail.com>
2015-01-24 02:56:40 +0100

    moving and jumping player

---
+++ JamSiders/Assets/Physics/CollisionChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Physics
+{
+    class CollisionChecker : MonoBehaviour
+    {
+        public bool debug;
+        private int collisionCount = 0;
+
+        public bool IsColliding { get { return collisionCount > 0; } }
+
+        void OnCollisionEnter()
+        {
+            if (debug) { Debug.Log("OnCollisionEnter, collisions: " + collisionCount, this); }
+            collisionCount++;
+        }
+
+        void OnCollisionExit()
+        {
+            if (debug) { Debug.Log("OnCollisionLeave, collisions: " + collisionCount, this);}
+            collisionCount--;
+        }
+
+    }
+}
