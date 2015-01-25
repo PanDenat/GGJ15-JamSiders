8cf2a1011dcab1151c9c97812c9412cbdc381104
PanDenat <pandenat@gmail.com>
2015-01-25 11:46:49 +0100

    managing skins for player and Ai

---
+++ JamSiders/Assets/Player/HideOnStart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Player
+{
+    class HideOnStart : MonoBehaviour
+    {
+        public MeshRenderer[] renderersToDisable;
+
+        void Start()
+        {
+            foreach (var c in renderersToDisable)
+            {
+                c.enabled = false;
+            }
+        }
+    }
+}
---
+++ JamSiders/Assets/Player/PrefabAttacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Player
+{
+    class PrefabAttacher : MonoBehaviour
+    {
+        public GameObject[] prefabsToAttach;
+
+        void Awake()
+        {
+            foreach (var prefab in prefabsToAttach)
+            {
+                var go = (GameObject)Instantiate(prefab);
+                go.transform.parent = transform;
+                go.transform.localPosition = prefab.transform.position;
+            }
+        }
+    }
+}
