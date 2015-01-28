f71c028c6e4ddcbae02ca691d25761789600d120
PanDenat <pandenat@gmail.com>
2015-01-28 14:45:26 +0100

    spinning with keyboard

--- JamSiders/Assets/Player/Spinner.cs
+++ JamSiders/Assets/Player/Spinner.cs
@@ -18,7 +18,15 @@ public class Spinner : MonoBehaviour
         rigidbody.maxAngularVelocity = maxRotation;
 
 	    var rot = Input.GetAxis("TriggerAxis");
+	    rot += GetRotationFromKeyboard();
 	    if (debug) { Debug.Log("Spinner TriggerAxis: " + rot);}
 	    rigidbody.AddRelativeTorque(Vector3.up * rot * rotScale * -1);
 	}
+
+    private float GetRotationFromKeyboard()
+    {
+        if (Input.GetButton("SpinLeft")) return -1;
+        if (Input.GetButton("SpinRight")) return 1;
+        return 0;
+    }
 }
